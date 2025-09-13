001e3d4568ea7c43d77a8b5301f42143c82b3a19
CaioZamana <caiobzm@gmail.com>
2025-09-13 01:41:09 -0300

    Golden Path: Reaact + Vite + PWA + TailWind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-green-700 text-white text-center text-3xl flex items-center justify-center">
+      ✅ Tailwind + PWA funcionando!
+    </div>
+  );
+}
+
