fde4002e6b51b4deb671a900feba9d86016a74fb
CaioZamana <caiobzm@gmail.com>
2025-09-15 20:07:22 -0300

    Commit para zenaide

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6n8p6hhus9o"
+    "revision": "0.uofub45819"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
